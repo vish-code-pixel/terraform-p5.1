7b3cb5facfd479b431ccf2630e314f0970485547
vish-code-pixel <121923658+vish-code-pixel@users.noreply.github.com>
2023-10-24 05:50:05 +0100

    Delete modules directory

--- modules/aks/aks_vars.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-    description =  "(Required) Keyvault Location"
-    default = "northeurope"  
-}
-
-variable "rg_name" {
-    description =  "Name of the Resource Group"
-    default = "techslate-ade-rg"  
-}
--- modules/keyvault/kv_outputs.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_id" {
-  description = "The id of the key vault"
-  value = azurerm_key_vault.key_vault.id
-}
-
-output "key_vault_name" {
-  description = "The name of the key vault"
-  value = azurerm_key_vault.key_vault.name
-}
